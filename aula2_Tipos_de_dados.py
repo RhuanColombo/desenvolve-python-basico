04afff4164d64da31fea320efd6fd11e2e45ca6d
RhuanColombo <162833520+RhuanColombo@users.noreply.github.com>
2024-06-08 14:01:20 -0300

    Add files via upload

---
+++ aula2_Tipos_de_dados.py
@@ -0,0 +1,35 @@
+#%% 1
+v1=5
+v2=2
+v3=v1/v2
+print(v3)
+print(type(v3))
+#%% 2
+v1="O resultado é"
+v2=10
+v3=3.5
+v4=v2+v3
+print(v1)
+print(type(v1))
+print(v2)
+print(type(v2))
+print(v3)
+print(type(v3))
+print(v4)
+print(type(v4))
+#%% 3
+v1=10
+v2=20
+aux=v2
+v2=v1
+v1=aux
+print(v1)
+print(v2)
+#%% 4
+juros=1.01
+saldo=500.0
+print("Após 3 meses meu novo saldo é")
+print(((saldo*juros)*juros)*juros)
+
+
+# %%
